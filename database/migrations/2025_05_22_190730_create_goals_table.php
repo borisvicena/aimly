08430b1eacc493efe1f5bdeb65cb352b44212169
Boris <boris.vicena@gmail.com>
2025-05-22 21:14:59 +0200

    created migration files for goals and tasks table

---
+++ database/migrations/2025_05_22_190730_create_goals_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * The name of the table to be created.
+     *
+     * @var string
+     */
+    protected string $tableName = 'goals';
+
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create($this->tableName, function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->date('deadline')->nullable();
+            $table->enum('status', ['active', 'completed', 'overdue'])->default('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists($this->tableName);
+    }
+};
